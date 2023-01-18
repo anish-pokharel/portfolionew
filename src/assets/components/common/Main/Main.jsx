ef78ae4c4b6cdc910cce128ec4ff5b948e1a1369
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2023-01-18 22:21:26 +0545

    8th

--- src/assets/components/common/Main/Main.jsx
+++ src/assets/components/common/Main/Main.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <div>
+      <h1>hello my name is anish Pokharel</h1>
+    </div>
+  );
 };
 
 export default Main;
